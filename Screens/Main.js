a42f568bd2ae82b3f5673d5568157823e79f6914
hussain-althobaiti <56229595+hussain-althobaiti@users.noreply.github.com>
2020-03-07 19:18:11 +0300

    home Icon

--- Screens/Main.js
+++ Screens/Main.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons,MaterialCommunityIcons ,AntDesign ,MaterialIcons} from '@expo/vector-icons';
+import { Ionicons,MaterialCommunityIcons ,AntDesign ,MaterialIcons,SimpleLineIcons} from '@expo/vector-icons';
 
 import Post from './Post';
 import Home from './Home';
@@ -19,7 +19,7 @@ import Chat from './Chat';
       
     return (
         <Tab.Navigator backBehavior="none" tabBarOptions={{activeTintColor: '#e91e63',showLabel: false, }}>
-          <Tab.Screen backBehavior="none" name="Home" component={Home} options={{tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home" color={color} size={size} />),}}/>
+          <Tab.Screen backBehavior="none" name="Home" component={Home} options={{tabBarIcon: ({ color, size }) => (<SimpleLineIcons name="home" color={color} size={size} />),}}/>
           <Tab.Screen name="Search" component={Search} options={{tabBarIcon: ({ color, size }) => (<Ionicons name="md-search" color={color} size={size} />),}}/>
           <Tab.Screen name="Post" component={Post} options={{tabBarIcon: ({ color, size }) => (<AntDesign name="pluscircleo" color={color} size={size} />),}}/>
           <Tab.Screen name="Profile" component={Profile} options={{tabBarIcon: ({ color, size }) => (<AntDesign name="user" color={color} size={size} />),}}/>
