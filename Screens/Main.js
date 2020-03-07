bf371e5e91aedba1cc260515f7dafd099d78dbd6
hussain-althobaiti <56229595+hussain-althobaiti@users.noreply.github.com>
2020-03-07 19:02:07 +0300

    backBehavior="none"

--- Screens/Main.js
+++ Screens/Main.js
@@ -18,8 +18,8 @@ import Chat from './Chat';
   const NarBar = () => {
       
     return (
-        <Tab.Navigator tabBarOptions={{activeTintColor: '#e91e63',showLabel: false, }}>
-          <Tab.Screen backBehavior={"none"} name="Home" component={Home} options={{tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home" color={color} size={size} />),}}/>
+        <Tab.Navigator backBehavior="none" tabBarOptions={{activeTintColor: '#e91e63',showLabel: false, }}>
+          <Tab.Screen backBehavior="none" name="Home" component={Home} options={{tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home" color={color} size={size} />),}}/>
           <Tab.Screen name="Search" component={Search} options={{tabBarIcon: ({ color, size }) => (<Ionicons name="md-search" color={color} size={size} />),}}/>
           <Tab.Screen name="Post" component={Post} options={{tabBarIcon: ({ color, size }) => (<AntDesign name="pluscircleo" color={color} size={size} />),}}/>
           <Tab.Screen name="Profile" component={Profile} options={{tabBarIcon: ({ color, size }) => (<AntDesign name="user" color={color} size={size} />),}}/>
